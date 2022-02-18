4a682d217181984e3e8ce2579391e148a688ef8e
Theo Olsson <theo.olsson1@learnet.se>
2022-02-18 11:13:11 +0100

    Konstruktor och första metoderna deklarerade
    Co-authored-by: Sofiia Löf <sofiia.lof@utb.ecutbildning.se>

--- Javascript/Pokemon.js
+++ Javascript/Pokemon.js
@@ -1,1 +1,32 @@
-export class Pokemon {}
+export class Pokemon {
+  constructor() {
+    this.baseUrl = "https://pokeapi.co/";
+    this.spritePath = "/api/v2/pokemon/";
+    this.flavorPath = "/api/v2/pokemon-species/";
+
+    this.url = new URL(this.baseUrl);
+
+    this.pokemons = this.GetAllPokemon(); //TODO Lagra enbart name och sprite
+  }
+
+  GetAllPokemon() {
+    //TODO api anrop som lagrar pokemon namn och sprite i en array till konstruktorn genom spritePath
+    //Theo
+  }
+
+  GetPokeName(index) {
+    //TODO hämta name från pokemons array
+    //Theo
+  }
+
+  GetPokeIcon(index) {
+    //TODO hämta sprite från pokemons array
+    //Theo
+  }
+
+  GetPokemonDescription(index) {
+    //TODO api anrop som hämtar specifik pokemons description genom flavorPath
+    //TODO returns pokemon beskrivning genom falvor_text_entries
+    //Sofiia
+  }
+}
