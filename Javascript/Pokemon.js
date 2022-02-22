d8f9afecd16d212f2f594a17a92613bf6e91fb1b
Theo Olsson <theo.olsson1@learnet.se>
2022-02-22 14:23:07 +0100

    Flyttat ut sorteringen utanför fetch loopen BUG - Get metoderna för name och icon retunerar fortfarande undefined

--- Javascript/Pokemon.js
+++ Javascript/Pokemon.js
@@ -8,25 +8,30 @@ export class Pokemon {
    */
   constructor(amount) {
     /**
-     * @type {string} domain part of the PokeAPI URL
+     * domain part of the PokeAPI URL
+     * @type {string}
      */
     this.baseUrl = "https://pokeapi.co/";
     /**
-     * @type {string} add a number to the string to get a specific pokemon based on the national pokedex and use as searchpath with the url. Base search path for basic information in the PokeAPI URL,
+     * add a number to the string to get a specific pokemon based on the national pokedex and use as searchpath with the url. Base search path for basic information in the PokeAPI URL
+     * @type {string}
      */
     this.spritePath = "/api/v2/pokemon/";
     /**
-     * @type {string} add a number to the string to get a specific pokemon based on the national pokedex and use as searchpath with the url. Base search path for description in the PokeAPI URL.
+     * add a number to the string to get a specific pokemon based on the national pokedex and use as searchpath with the url. Base search path for description in the PokeAPI URL.
+     * @type {string}
      */
     this.flavorPath = "/api/v2/pokemon-species/";
 
     /**
-     * @type {URL} the base URL to access PokeAPI
+     * the base URL to access PokeAPI
+     * @type {URL}
      */
     this.url = new URL(this.baseUrl);
 
     /**
-     * @type {Array.<{dexId: number, name: string, sprite: string, height: number, weight: number, dualType: boolean, typePrimary: string, typeSecondary: string|void}>} objects containg information about a Pokemon. Size values are  are in centimeters and grams. typeSecondary will only be defined if dualType equals true
+     * objects containg information about a Pokemon. Size values are  are in centimeters and grams. typeSecondary will only be defined if dualType equals true
+     * @type {Array.<{dexId: number, name: string, sprite: string, height: number, weight: number, dualType: boolean, typePrimary: string, typeSecondary: string|void}>}
      */
     this.pokemons = this.SetPokemonArray(amount);
   }
@@ -63,9 +68,10 @@ export class Pokemon {
 
           pokemonObject.push(pokemon);
           return object;
-        })
-        .then(() => pokemonObject.sort((a, b) => a.dexId - b.dexId));
+        });
     }
+
+    pokemonObject.sort((a, b) => a.dexId - b.dexId);
     console.log(pokemonObject); //TODO radera, enbart hjälp vid skapande av metod
     return pokemonObject;
   }
