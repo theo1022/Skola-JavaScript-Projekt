5cc47c2655614f93ef5e8ef5fa6cf7bfda1c123a
Sofiia Löf <sofiia.lof@utb.ecutbildning.se>
2022-02-23 11:58:39 +0100

    Fixar till små detaljer i modal

--- Javascript/script.js
+++ Javascript/script.js
@@ -73,7 +73,7 @@ async function showDescription(name, pokeArray) {
 
     if (cardTitle.innerText.toLowerCase() === name) {
       pokemonName.innerText = cardTitle.innerText;
-      pokemonId.innerHTML = "DexID: " + pokeArray[index].dexId;
+      pokemonId.innerHTML = "National dex # " + pokeArray[index].dexId;
       pokemonHeightandWeight.innerText = "Height: " + pokeArray[index].height + " Weight: " + pokeArray[index].weight;
       pokemonImage.src = cardImage.src;
       typeCollection.innerHTML = cardTypeCollection.innerHTML;
