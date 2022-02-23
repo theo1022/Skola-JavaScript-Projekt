527bc78f7496645277c192881f93bfeedfff295c
Theo Olsson <theo.olsson1@learnet.se>
2022-02-23 01:27:09 +0100

    Skriver ut pokemon

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,6 +4,34 @@ import { Pokemon } from "./Pokemon.js";
 const pokemon = new Pokemon();
 
 //Använd klass objektet för att kalla på metoden som retunerar en array med objekt från api:n
-const array = await pokemon.GetPokemonArray(1, 6);
 
-console.log(array[0].name);
+PrintPokeCard(1, 6);
+
+async function PrintPokeCard(dexNumberStart, amount) {
+  const array = await pokemon.GetPokemonArray(dexNumberStart, amount);
+  console.log(array); //TODO radera, enbart hjälp vid skapande av metod
+
+  const cards = document.querySelectorAll(".card");
+
+  for (let card of cards) {
+    const index = card.id.slice(5, 6) - 1; //TODO Fråga Sofiia om vi kan döpa om id för card så deras nummer är 0-5 istället för 1-6
+    console.log(index);
+
+    const nameElem = card.querySelector(".card-title");
+    nameElem.innerHTML = array[index].name;
+    nameElem.innerHTML = array[index].name.toUpperCase();
+
+    const imgElem = card.querySelector(`#icon-${index + 1}`); //TODO rätta index?
+    imgElem.src = array[index].spriteUrl; //TODO Ändra max storlek på bilden till 96*96
+
+    const priceElem = card.querySelector(".card-text"); //TODO skapa ett typeElem i index.html
+    priceElem.innerHTML = `<div class="type ${
+      array[index].typePrimary
+    }">${array[index].typePrimary.toUpperCase()}</div>`;
+    if (array[index].dualType) {
+      priceElem.innerHTML += `<div class="type ${
+        array[index].typeSecondary
+      }">${array[index].typeSecondary.toUpperCase()}</div>`;
+    }
+  }
+}
